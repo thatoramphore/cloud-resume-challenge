7802389f9376b5423eec296e8859b9f695623a63
thatoramphore <r2.thato@gmail.com>
2023-04-22 15:14:17 +0200

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,47 @@
+/*!
+* Start Bootstrap - Resume v7.0.5 (https://startbootstrap.com/theme/resume)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+//  Update visit counter
+const counter = document.querySelector(".visit-counter");
+
+async function updateCounter() {
+  let response = await fetch("https://cjri3u5jhg43jjpi4cghleggcm0kpstl.lambda-url.af-south-1.on.aws/");
+  let data = await response.json();
+  console.log("Response data: ", data);
+  counter.innerHTML = `You're visitor number: ${data}!`;
+} 
+
+updateCounter();
+
